65d448dcdbaab9da4555854a7b417d009bdcf432
yaoyaomua <48670274+yaoyaomua@users.noreply.github.com>
2022-11-11 06:47:14 +0800

    merge

--- src/LoRaEnergyModules/LoRaAppEnergyConsumer.h
+++ src/LoRaEnergyModules/LoRaAppEnergyConsumer.h
@@ -1,29 +1,17 @@
-<<<<<<< Updated upstream
-//
-=======
     //
->>>>>>> Stashed changes
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-<<<<<<< Updated upstream
-// 
-=======
 //
->>>>>>> Stashed changes
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-<<<<<<< Updated upstream
-// 
-=======
 //
->>>>>>> Stashed changes
 
 #ifndef LORAENERGYMODULES_LORAENERGYCONSUMER_H_
 #define LORAENERGYMODULES_LORAENERGYCONSUMER_H_
@@ -37,7 +25,7 @@ using namespace inet;
 
 namespace flora {
 
-class LoRaEnergyConsumer: public inet::physicallayer::StateBasedEpEnergyConsumer {
+class LoRaAppEnergyConsumer: public inet::physicallayer::StateBasedEpEnergyConsumer {
 public:
     void initialize(int stage) override;
     void finish() override;
